8d7102ac5d323c1ce5ca2d89882440f5d6009c32
vitormoschetta <vitormoschetta@gmail.com>
2020-12-11 21:14:37 -0400

    add project

---
+++ Domain/Settings.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Domain
+{
+    public static class Settings
+    {
+        public static string ConnectionString()
+        {
+            return "DataSource=../DataBase.db";
+            
+            //return "Server=172.16.107.164,2733;DataBase=FeraJoer;User id=dba;Password=m4st3rpw1";
+        }
+
+
+        public static string Secret = "fedaf7d8863b48e197b9287d492b708e";
+
+    }
+}
